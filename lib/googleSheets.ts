939a5f343770fcc89c75d58752ce15001953d3f5
TDtarbo <rmtdwijerathna2001@gmail.com>
2026-04-05 16:13:26 +0530

    fix build error

--- lib/googleSheets.ts
+++ lib/googleSheets.ts
@@ -53,11 +53,6 @@ export function createGoogleSheetsClient() {
 			private_key: privateKey,
 			client_email: clientEmail,
 			client_id: process.env.GOOGLE_CLIENT_ID,
-			auth_uri: process.env.GOOGLE_AUTH_URI,
-			token_uri: process.env.GOOGLE_TOKEN_URI,
-			auth_provider_x509_cert_url:
-				process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-			client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
 			universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
 		},
 		scopes: ["https://www.googleapis.com/auth/spreadsheets"],
